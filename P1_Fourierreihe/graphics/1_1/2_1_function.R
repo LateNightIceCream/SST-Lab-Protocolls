a6f5ec6dfdb9f1cd8f6cabf5cd08ce3262bc232e
LateNightIceCream <richardgruenert@live.com>
2019-11-01 10:09:18 +0100

    preamble file

--- P1_Fourierreihe/graphics/1_1/2_1_function.R
+++ P1_Fourierreihe/graphics/1_1/2_1_function.R
@@ -18,7 +18,7 @@ xlabel <- bquote( "t" )
 
 
 xbreaks <- c(0, -tau/2, tau/2, -T1/2, T1/2, -T1, T1)
-xlabels <- c(0, bquote("-"~over(tau, 2)), bquote(over(tau, 2)), bquote("-"~over("T"[1], 2)), bquote(over("T"[1], 2)), bquote("-T"[1]), bquote("T"[1]))
+xlabels <- c(0, bquote("-"~over(tau, 2)), bquote(over(tau, 2)), bquote("-"~over("T", 2)), bquote(over("T", 2)), bquote("-T"), bquote("T"))
 
 #for(n in 1:3) {
   #xbreaks <- c(xbreaks, n*T1)
@@ -30,7 +30,7 @@ function_color <- "#0CB5DF"
 
 ####################################################################
 
-p <- ggplot(data.frame(x=c(0,2), y=c(0,2)), aes(x=x)) +
+p <- ggplot(data.frame(x=c(0,2), y=c(-2,2)), aes(x=x)) +
 
         theme_minimal() + # maybe theme_bw()
         theme(legend.position="none", plot.title = element_text(color = "gray21", size=1.6180339887498948^5), plot.subtitle = element_text(color = "grey80", size=1.6180339887498948^5)) +
@@ -41,7 +41,7 @@ p <- ggplot(data.frame(x=c(0,2), y=c(0,2)), aes(x=x)) +
         xlab(xlabel) +
         #ylab(ylabel)  +
 
-        scale_y_continuous(limits=c(0, 1.6180339887498948), breaks=c(0,A, 1.6180339887498948), labels=c(0, "A", "")) +
+        scale_y_continuous(limits=c(-1.618033, 1.6180339887498948), breaks=c(-A,0,A), labels=c("-A", 0, "A")) +
         scale_x_continuous(limits=c(-epsilon, epsilon), breaks=xbreaks, labels=xlabels)
         #scale_x_discrete(limits=1, breaks=c(1,2,3)) #labels=c(0, bquote("-"~over("T1",2)), bquote(over(-tau,2)), bquote(over(-tau,2)), bquote(over(tau,2))) )
 
@@ -51,15 +51,15 @@ for (n in -1:1) {
 
   # solid horizontal lines
   p <- p + geom_segment( x = -tau/2 + n*delta, y = A, xend = tau/2  + n*delta,  yend = A, color=function_color, linetype="solid") +
-      geom_segment( x = -T1/2  + n*delta, y = 0, xend = -tau/2 + n*delta,  yend = 0, color=function_color, linetype="solid") +
-      geom_segment( x = T1/2   + n*delta, y = 0, xend = tau/2  + n*delta,  yend = 0, color=function_color, linetype="solid")
+      geom_segment( x = -T1/2  + n*delta, y = -A, xend = -tau/2 + n*delta,  yend = -A, color=function_color, linetype="solid") +
+      geom_segment( x = T1/2   + n*delta, y = -A, xend = tau/2  + n*delta,  yend = -A, color=function_color, linetype="solid")
 
   # test segment
   #p <- p + geom_segment( x = T1/2, y = 0, xend = T1/2-tau/2 + T1/2, yend = 0, aes(color="blue"), linetype="solid")
 
   # dashed vertical lines
-  p <- p + geom_segment( x = -tau/2 + n*delta, y = 0, xend = -tau/2 + n*delta, yend = A, color=function_color, linetype="dashed")+
-           geom_segment( x =  tau/2 + n*delta, y = 0, xend = tau/2 + n*delta,  yend = A, color=function_color, linetype="dashed")
+  p <- p + geom_segment( x = -tau/2 + n*delta, y = -A, xend = -tau/2 + n*delta, yend = A, color=function_color, linetype="dashed")+
+           geom_segment( x =  tau/2 + n*delta, y = -A, xend = tau/2 + n*delta,  yend = A, color=function_color, linetype="dashed")
 }
 
 pdf("1_1_function.pdf", width = output_width, height = output_height, )
